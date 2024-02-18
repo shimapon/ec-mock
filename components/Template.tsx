d857137f792cf56c9ff158083b2017f3fbd70b4f
shimapon <160441081@ccalumni.meijo-u.ac.jp>
2024-02-18 14:42:48 +0900

    lint

--- components/Template.tsx
+++ components/Template.tsx
@@ -1,10 +1,10 @@
 import { Header } from "./Header";
-import { ItemImage } from "./main/ItemImage";
-import { Title } from "./main/Title";
-import { Price } from "./main/Price";
 import { ButtonArea } from "./main/AddCart";
+import { ItemImage } from "./main/ItemImage";
 import { ItemInfo } from "./main/ItemInfo";
+import { Price } from "./main/Price";
 import { ThumbnailList } from "./main/ThumbnailList";
+import { Title } from "./main/Title";
 import styles from "./styles.module.scss";
 
 export const Template: React.FC = () => {
